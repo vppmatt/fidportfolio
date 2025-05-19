750347ee2f12f7513ddde0d082cd4e26389dc447
Matt <matt.thornfield@neueda.com>
2025-05-19 13:03:50 +0000

    portfolio

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import domain.Order;
+import domain.OrderType;
 import domain.Stock;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -10,9 +16,16 @@ public class Main {
 
         Stock apple = new Stock("Apple", "APPL");
 
-        System.out.println(apple);
+        ArrayList<Order> portfolio = new ArrayList<>();
 
-        System.out.println(apple == microsoft); //not what we want
-        System.out.println(apple.equals(microsoft));
+        BigDecimal num1 = new BigDecimal("123.45"); //OK
+        BigDecimal num2 = BigDecimal.valueOf(123.45); //OK
+        BigDecimal num3 = new BigDecimal(123.45); //AVOID - inaccurate
+
+        portfolio.add(new Order(microsoft, LocalDateTime.now(), OrderType.PURCHASE, new BigDecimal("454.27"),100));
+        portfolio.add(new Order(microsoft, LocalDateTime.now(), OrderType.SALE, new BigDecimal("456.73"),30));
+        portfolio.add(new Order(apple, LocalDateTime.now(), OrderType.PURCHASE, new BigDecimal("200.74"),100));
+        portfolio.add(new Order(apple, LocalDateTime.now(), OrderType.PURCHASE, new BigDecimal("195.33"),50));
+
     }
 }
